cbd00d9f9d6e9e3bcf518db42c493ea5d3b55b84
Ricqrdo <ricqrdo-@hotmail.com>
2020-09-21 17:23:48 -0500

    trying cors

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,9 +1,8 @@
 const jwt = require("jsonwebtoken");
-const { restart } = require("nodemon");
 
 module.exports = function (req, res, next) {
   const token = req.header("auth-token");
-  if (!token) return restart.status(401).send("Access Denied");
+  if (!token) return res.status(401).send("Access Denied");
 
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);

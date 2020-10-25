e5da69928bf17fe6de9b69d9eb766bc6821ce7bd
ricardo <ricqrdo-@hotmail.com>
2020-10-25 17:00:53 -0500

    Added allowed headers in cors config

--- src/index.js
+++ src/index.js
@@ -15,7 +15,11 @@ const authRoute = require("./routes/user.routes");
 // middlewares
 app.use(express.urlencoded({limit: '50mb', extended: true }));
 app.use(express.json({limit: '50mb'}));
-app.use(cors({ credentials: true, origin: ["http://localhost:3000", "https://lectortmo.netlify.app/"] }));
+app.use(cors({ 
+  credentials: true, 
+  origin: ["http://localhost:3000", "https://lectortmo.netlify.app/"],
+  allowedHeaders: ['Content-Type', 'Authorization'] 
+}));
 app.use(cookieParser());
 app.use("/api", filesRoutes);
 app.use("/user", authRoute);
